174a68ec47c29ed9d744f89cd624b349509c1cde
Carlos Enrique Llamosas Duarte <carlos.llamosas@arquinfo.com>
2020-03-12 18:46:14 -0500

    login part1

---
+++ pages/login.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="flex min-h-screen justify-center">
+    <form class="w-full max-w-sm self-center">
+      <div class="md:flex md:items-center mb-6 text-6xl">
+        <p class="mx-auto">SocialApp</p>
+      </div>
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3">
+          <label
+            class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            for="inline-use"
+          >User</label>
+        </div>
+        <div class="md:w-2/3">
+          <input
+            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            id="inline-user"
+            type="text"
+            value
+          />
+        </div>
+      </div>
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3">
+          <label
+            class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            for="inline-pass"
+          >Pass</label>
+        </div>
+        <div class="md:w-2/3">
+          <input
+            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            id="inline-pass"
+            type="password"
+            placeholder
+          />
+        </div>
+      </div>
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3"></div>
+        <label class="md:w-2/3 block text-gray-500 font-bold">
+          <a href="">Forgot password?</a>
+        </label>
+      </div>
+      <div class="md:flex md:justify-center">
+        <div class="w-1/2">
+          <button
+            class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            type="button"
+          >Register</button>
+        </div>
+        <div class="w-2/2">
+          <button
+            class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            type="button"
+          >Login</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
